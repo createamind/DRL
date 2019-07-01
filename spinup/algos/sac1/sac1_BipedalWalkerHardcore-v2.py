c8f93a22a948b575c29d68e7fa4fdcecca10bb1c
liu <supernova.bin@gmail.com>
2019-07-01 15:14:10 +0800

    plot.py

--- spinup/algos/sac1/sac1_BipedalWalkerHardcore-v2.py
+++ spinup/algos/sac1/sac1_BipedalWalkerHardcore-v2.py
@@ -6,6 +6,7 @@ import time
 from spinup.algos.sac1 import core
 from spinup.algos.sac1.core import get_vars
 from spinup.utils.logx import EpochLogger
+from gym.spaces import Box, Discrete
 
 
 class ReplayBuffer:
@@ -370,11 +371,51 @@ if __name__ == '__main__':
             for _ in range(3):
                 obs_, reward_, done_, info_ = self._env.step(action)
                 r = r + reward_
+                r -= 0.001
             return obs_, r, done_, info_
+
+
+    class Env_wrapper(gym.Env):
+
+        def __init__(self, env, flag="obs", action_repeat=1):
+            self.env = gym.make(env)
+            self.action_repeat = action_repeat
+            self.flag = flag
+            self.action_space = self.env.action_space
+            self.observation_space = self.env.observation_space
+            if self.flag == "obs_act":
+                # print("<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+                self.act_dim = self.action_space.shape[0]
+                self.obs_dim = self.action_space.shape[0] + self.env.observation_space.shape[0]
+                self.observation_space = Box(-np.inf, np.inf, shape=(self.obs_dim,), dtype=np.float32)
+
+        def reset(self):
+            obs = self.env.reset()
+            if self.flag == "obs_act":
+                obs = np.append(obs, np.zeros(self.act_dim))
+            return obs
+
+        def step(self, action):
+            reward = 0.0
+            for _ in range(self.action_repeat):
+                obs, r, done, info = self.env.step(action)
+                r -= 0.001  # punishment for stay still
+                reward += r
+            # reward -= 0.001
+            if self.flag == "obs_act":
+                obs = np.append(obs, action.reshape(self.act_dim))
+            reward = np.clip(reward, -50, 1000)
+            return obs, reward, done, info
+
+        def render(self):
+            self.env.render()
+
+
     
-    env = Wrapper(env)
+    # env = Env_wrapper(args.env, 'obs_act', 3)
+    env = Wrapper(gym.make(args.env))
 
     sac1(lambda : env, actor_critic=core.mlp_actor_critic,
-        ac_kwargs=dict(hidden_sizes=[128,128,128]),
+        ac_kwargs=dict(hidden_sizes=[400,300]),
         gamma=args.gamma, seed=args.seed, epochs=args.epochs, alpha=args.alpha,
         logger_kwargs=logger_kwargs)
