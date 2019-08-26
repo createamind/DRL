ca9cf05794f3e0a32f61effeb54727b222801179
zlw21gxy <zlw21gxy@gmail.com>
2019-08-26 16:53:35 +0800

    random

--- spinup/algos/sac1/sac1.py
+++ spinup/algos/sac1/sac1.py
@@ -47,7 +47,7 @@ Soft Actor-Critic
 """
 def sac1(env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), seed=0,
         steps_per_epoch=5000, epochs=100, replay_size=int(1e6), gamma=0.99, 
-        polyak=0.995, lr=1e-3, alpha=0.2, batch_size=100, start_steps=10000,
+        polyak=0.995, lr=1e-4, alpha=0.2, batch_size=150, start_steps=10000,
         max_ep_len=1000, logger_kwargs=dict(), save_freq=1):
     """
 
@@ -309,11 +309,11 @@ def sac1(env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), seed=0,
             epoch = t // steps_per_epoch
 
             # Save model
-            if (epoch % save_freq == 0) or (epoch == epochs-1):
-                logger.save_state({'env': env}, None)
+            # if (epoch % save_freq == 0) or (epoch == epochs-1):
+            #     logger.save_state({'env': env}, None)
 
             # Test the performance of the deterministic version of the agent.
-            test_agent()
+            test_agent(25)
 
             # logger.store(): store the data; logger.log_tabular(): log the data; logger.dump_tabular(): write the data
             # Log info about epoch
@@ -323,7 +323,7 @@ def sac1(env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), seed=0,
             logger.log_tabular('EpLen', average_only=True)
             logger.log_tabular('TestEpLen', average_only=True)
             logger.log_tabular('TotalEnvInteracts', t)
-            logger.log_tabular('Alpha',average_only=True)
+            logger.log_tabular('Alpha', average_only=True)
             logger.log_tabular('Q1Vals', with_min_and_max=True) 
             logger.log_tabular('Q2Vals', with_min_and_max=True) 
             # logger.log_tabular('VVals', with_min_and_max=True)
@@ -339,16 +339,20 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     # parser.add_argument('--env', type=str, default='BipedalWalkerHardcore-v2')  # 'Pendulum-v0'
-
-    # parser.add_argument('--env', type=str, default='Humanoid-v2')
-    parser.add_argument('--env', type=str, default='LunarLanderContinuous-v2')
+    parser.add_argument('--env', type=str, default='Humanoid-v2')
+    # parser.add_argument('--env', type=str, default='LunarLanderContinuous-v2')
     parser.add_argument('--hid', type=int, default=300)
     parser.add_argument('--l', type=int, default=1)
     parser.add_argument('--gamma', type=float, default=0.99)
     parser.add_argument('--seed', '-s', type=int, default=0)
-    parser.add_argument('--epochs', type=int, default=100)
-    parser.add_argument('--alpha', default=0.1, help="alpha can be either 'auto' or float(e.g:0.2).")
-    parser.add_argument('--exp_name', type=str, default='sac1_lunar-v0')
+    parser.add_argument('--epochs', type=int, default=10000)
+    parser.add_argument('--alpha', default=0.2, help="alpha can be either 'auto' or float(e.g:0.2).")
+    # parser.add_argument('--exp_name', type=str, default='sac1_Humanoid-v2_baseline')
+    name = 'sac1_baseline_{}_alpha_{}_seed_{}'.format(
+        parser.parse_args().env,
+        parser.parse_args().alpha,
+        parser.parse_args().seed)
+    parser.add_argument('--exp_name', type=str, default=name)  
     args = parser.parse_args()
 
     from spinup.utils.run_utils import setup_logger_kwargs
