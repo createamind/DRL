b2ee8fb1b25a8f72182f5b3324fa762e4ee37d7f
liu <supernova.bin@gmail.com>
2019-09-12 10:11:30 +0800

    randcomm

--- spinup/algos/maxsqn/maxsqn_nsteps_football.py
+++ spinup/algos/maxsqn/maxsqn_nsteps_football.py
@@ -219,7 +219,7 @@ def maxsqn(args, env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), s
     if alpha == 'auto':
         # target_entropy = (-np.prod(env.action_space.n))
         # target_entropy = (np.prod(env.action_space.n))/4/10
-        target_entropy = 0.5
+        target_entropy = args.target_entropy
 
         log_alpha = tf.get_variable('log_alpha', dtype=tf.float32, initializer=0.0)
         alpha = tf.exp(log_alpha)
@@ -253,8 +253,11 @@ def maxsqn(args, env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), s
 ######
 
     # Min Double-Q:
-    min_q_pi = tf.minimum(q1_pi_, q2_pi_)        # x2
-    # min_q_pi = tf.minimum(q1_mu_, q2_mu_)
+    if args.use_max:
+        min_q_pi = tf.minimum(q1_mu_, q2_mu_)
+    else:
+        min_q_pi = tf.minimum(q1_pi_, q2_pi_)        # x2
+
 
     # min_q_pi = tf.clip_by_value(min_q_pi, 0.0, 200.0)
 
@@ -574,7 +577,7 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     #  {'academy_empty_goal':32, 'academy_3_vs_1_with_keeper':44, 'academy_single_goal_versus_lazy':108}
-    parser.add_argument('--env', type=str, default='academy_single_goal_versus_lazy_random') # 'academy_3_vs_1_with_keeper_random')#
+    parser.add_argument('--env', type=str, default='academy_3_vs_1_with_keeper_random')#'academy_single_goal_versus_lazy_random') #
     parser.add_argument('--epochs', type=int, default=200000)
     parser.add_argument('--steps_per_epoch', type=int, default=int(5e3))
     parser.add_argument('--save_freq', type=int, default=10)
@@ -597,8 +600,10 @@ if __name__ == '__main__':
 
     parser.add_argument('--max_ep_len', type=int, default=500)    # make sure: max_ep_len < steps_per_epoch
     parser.add_argument('--alpha', default='auto', help="alpha can be either 'auto' or float(e.g:0.2).")
+    parser.add_argument('--target_entropy', type=float, default=0.4)
+    parser.add_argument('--use_max', type=bool, default=False)
     parser.add_argument('--lr', type=float, default=5e-5)
-    parser.add_argument('--exp_name', type=str, default='lazy_random_incentive') #'pi_3v1_auto_random')# ')#'1_{}_seed{}-0-half-random_repeat2'.format(parser.parse_args().env,parser.parse_args().seed))
+    parser.add_argument('--exp_name', type=str, default='3v1_random_incentive') #'pi_3v1_auto_random')# ')#'1_{}_seed{}-0-half-random_repeat2'.format(parser.parse_args().env,parser.parse_args().seed))
     args = parser.parse_args()
 
     from spinup.utils.run_utils import setup_logger_kwargs
