90578c2447d47da661269cb6c981fd04fe2977f9
JingbinLiu <liujingbin08@163.com>
2020-12-09 23:04:50 +0800

    gsqn_h0.35 & gsqn_auto0.35

--- spinup/algos/maxsqn/LunarLander-v2/gsqn.py
+++ spinup/algos/maxsqn/LunarLander-v2/gsqn.py
@@ -53,8 +53,8 @@ Soft Actor-Critic
 """ make sure: max_ep_len < steps_per_epoch """
 
 def maxsqn(env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), seed=0,
-        steps_per_epoch=500, epochs=200, replay_size=int(1e6), gamma=0.99,
-        polyak=0.995, lr=1e-3, alpha=0.2, batch_size=200, start_steps=100,
+        steps_per_epoch=5000, epochs=200, replay_size=int(1e6), gamma=0.99,
+        polyak=0.995, lr=1e-3, alpha=0.2, batch_size=200, start_steps=5000,
         max_ep_len=1000, logger_kwargs=dict(), save_freq=1):
     """
 
@@ -160,7 +160,7 @@ def maxsqn(env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), seed=0,
     if alpha == 'auto':
         # target_entropy = (-np.prod(env.action_space.n))
         # target_entropy = (np.prod(env.action_space.n))/4/10
-        target_entropy = 0.4
+        target_entropy = 0.35
 
         log_alpha = tf.get_variable('log_alpha', dtype=tf.float32, initializer=0.0)
         alpha = tf.exp(log_alpha)
@@ -257,7 +257,8 @@ def maxsqn(env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), seed=0,
         logps = log_softmax(np.array(logits) / alpha)
         return -np.sum(np.exp(logps) * logps)
 
-    entf = lambda x, logits: x * entropy(logits, x) - 0.15
+    # entf = lambda x, logits: x * entropy(logits, x) - 0.3
+    entf = lambda x, logits: entropy(logits, x) - 0.35
 
     def softmax_ud(logits):
         logps = log_softmax(np.array(logits))
@@ -268,7 +269,11 @@ def maxsqn(env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), seed=0,
             return sess.run(mu, feed_dict={x_ph: np.expand_dims(o, axis=0)})[0]
         else:
             q_logits = sess.run(v_x, feed_dict={x_ph: np.expand_dims(o, axis=0)})[0]
-            alpha_ad = optimize.bisect(entf, args=q_logits,a=0.0001, b=10000, xtol=1e-4, rtol=1e-5)
+            try:
+                alpha_ad = optimize.bisect(entf, args=q_logits,a=0.0001, b=10000, xtol=1e-4, rtol=1e-5)
+            except:
+                print(q_logits)
+                alpha_ad = 0.0001
             # alpha_ad = min(1.0, alpha_ad)
             # print(alpha_ad)
             # print(alpha_ad * entropy(q_logits, alpha_ad))
@@ -418,11 +423,11 @@ if __name__ == '__main__':
     parser.add_argument('--l', type=int, default=1)
     parser.add_argument('--gamma', type=float, default=0.99)
     parser.add_argument('--seed', '-s', type=int, default=1)
-    parser.add_argument('--epochs', type=int, default=5000)
-    parser.add_argument('--max_ep_len', type=int, default=1000)    # make sure: max_ep_len < steps_per_epoch
+    parser.add_argument('--epochs', type=int, default=50000)
+    parser.add_argument('--max_ep_len', type=int, default=5000)    # make sure: max_ep_len < steps_per_epoch
     parser.add_argument('--alpha', default=0.2, help="alpha can be either 'auto' or float(e.g:0.2).")
     parser.add_argument('--lr', type=float, default=1e-3)
-    parser.add_argument('--exp_name', type=str, default='maxsqn_LunarLander-v2_debug')
+    parser.add_argument('--exp_name', type=str, default='gsqn_h0.35_400-300')
     args = parser.parse_args()
 
 
@@ -452,7 +457,7 @@ if __name__ == '__main__':
     env_lunar1 = gym.make(args.env)
     env_lunar3 = Wrapper(gym.make(args.env),3)
     # env_lunar = gym.make(args.env)
-    maxsqn(lambda n : env_lunar3 if n==3 else env_lunar1, actor_critic=core.mlp_actor_critic,
-        ac_kwargs=dict(hidden_sizes=[128,128]),
+    maxsqn(lambda n : env_lunar1 if n==3 else env_lunar1, actor_critic=core.mlp_actor_critic,
+        ac_kwargs=dict(hidden_sizes=[400,300]),
         gamma=args.gamma, seed=args.seed, epochs=args.epochs, alpha=args.alpha, lr=args.lr, max_ep_len = args.max_ep_len,
         logger_kwargs=logger_kwargs)
